6a3d2a907ea535356f3070d9b2a1e39364e85d32
Derek Bourgeois <derek@devoption.io>
2019-04-30 02:08:33 -0400

    make fields fillable

--- app/Leave.php
+++ app/Leave.php
@@ -6,6 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Leave extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'employee_id',
+        'leave_type',
+        'date_from',
+        'date_to',
+        'days',
+        'reason'
+    ];
+
     public function users()
     {
         return $this->belongsTo('App\User', 'employee_id');
