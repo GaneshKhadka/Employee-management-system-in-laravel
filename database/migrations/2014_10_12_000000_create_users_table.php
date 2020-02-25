6f48d3b92d25ea3401a8a25c3d932e1d7e5d7918
Rahul Dey <rahul900day@gmail.com>
2020-02-25 14:11:02 +0530

    Unique Key Added on Username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('leaves_count')->nullable();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->text('image')->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
